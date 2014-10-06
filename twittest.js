022e3c245ed4b46cc748959a6a8d64e1db469ba2
adamdiy <adamdiy@gmail.com>
2014-10-06 17:48:57 +0100

    working prototype

--- twittest.js
+++ twittest.js
@@ -11,15 +11,19 @@ var T = new Twit({
 });
 
 console.log("Reading in the last 20 tweets with search: #collectiveacademy");
-T.get('search/tweets', { q: 'collectiveacademy', count: 50, media: true }, function(err, reply) {
+T.get('search/tweets', { q: 'collectiveacademy', count: 20 }, function(err, reply) {
     if (err) {
         console.dir(err);
     } else {
         for (var i = 0; i < reply.statuses.length; i++) {
               var status = reply.statuses[i];
-              toWrite += "<img src='";
+
+              toWrite += '  username: ' + status.user.name;
+              toWrite += '  time/date: ' + status.created_at;
+              toWrite += "<br><img src='";
               toWrite += status.entities.media[0].media_url;
-              toWrite += "'>";
+//            console.log(status.entities.media[0].media_url);
+              toWrite += "'><br>";
               
 //            if (i===reply.statuses.length) toWrite += "'><";
 //            console.log('*************************');
@@ -27,7 +31,21 @@ T.get('search/tweets', { q: 'collectiveacademy', count: 50, media: true }, funct
 //            console.log('   ' + status.text);
 //            console.log('  time/date: ' + status.created_at);
 //            console.log('*************************');
+//            console.log(status);
+          
         }
-      console.log(toWrite);
+//
     }
 });
+
+
+var server = Hapi.createServer('localhost', Number(process.argv[2]) || 8080);
+
+server.route({path: '/', method:'GET', handler: function handler(request, reply) {
+ reply("<html><head><title>"+ "Sup"+"</title><body>" + toWrite + "</body></html>");
+   }
+
+});
+
+server.start();
+console.log("server going");
