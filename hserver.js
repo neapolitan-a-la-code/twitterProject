9b3bc3a6bb7526655c9eb6e22009a33f89178d69
adamdiy <adamdiy@gmail.com>
2014-10-07 15:01:18 +0100

    tserver2 (twit serve) and hserver (human server)

---
+++ hserver.js
@@ -0,0 +1,28 @@
+var http = require("http");
+var imagecode = "";
+
+var options = {
+  host: '127.0.0.1',
+  port: 8080,
+  path: '/'
+};
+
+http.createServer(function(request,response){
+	console.log("I got kicked");
+
+
+  http.get(options, function(res) {
+//    console.log("Got response: " + res.statusCode);
+
+    res.on("data", function(chunk) {
+      imagecode = chunk;
+  
+    });
+  }).on('error', function(e) {
+    console.log("Got error: " + e.message);
+  });
+
+	response.writeHeader(200, {"Content-Type": "text/plain"});
+	response.end(imagecode);
+}).listen(7070);
+console.log("Server Running on 7070");	
--- tserver2.js
+++ tserver2.js
@@ -1,8 +1,7 @@
 var Twit = require('twit');
-var Hapi = require('hapi');
-var toWrite = "";
+var http = require("http");
 var credentials = require('./credentials.js');
-
+var toWrite = "";
 
 var T = new Twit({
     consumer_key: credentials.consumer_key,
@@ -10,7 +9,6 @@ var T = new Twit({
     access_token: credentials.access_token,
     access_token_secret: credentials.access_token_secret
 });
-
 
 T.get('search/tweets', { q: 'collectiveacademy', count: 20 }, function(err, reply) {
 
@@ -39,18 +37,11 @@ T.get('search/tweets', { q: 'collectiveacademy', count: 20 }, function(err, repl
 });
     
 
-var server = Hapi.createServer('localhost', Number(process.argv[2]) || 8080);
-
-server.route({path: '/', method:'GET', handler: function handler(request, reply) {
 
 
-    reply.end(toWrite);
-
-
-//  reply("<html><head><title>"+ "Sup"+"</title><body>" + toWrite + "</body></html>");
-   }
-
-});
-
-server.start();
-console.log("Node server up at " + server.info.uri + ", searching: #collectiveacademy");
+http.createServer(function(request,response){
+	console.log("I got kicked");
+	response.writeHeader(200, {"Content-Type": "text/plain"});
+	response.end(toWrite);
+}).listen(8080);
+console.log("Server Running on 8080");	
