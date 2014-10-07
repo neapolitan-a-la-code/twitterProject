a7f42f4b790575e947e314eef868e1b60c4530e4
NataliaLKB <natalialeebaltazar@gmail.com>
2014-10-07 10:44:12 +0100

    updating .gitignore

--- credentials.js
+++
@@ -1,8 +0,0 @@
-var credentials = {
-	consumer_key: 'aTIpG0jGl6nwp4KWhUILBQ',
-    consumer_secret: 'cUmQNsQ6u4wn4PUcoQ9PmqzFEzVmzJmGnD2gROeY',
-    access_token: '44984017-EKeo9ivt957w7Y6uiT2NsCFbM1PQ46KF0c5J41egQ',
-    access_token_secret:  'DvTKLNq0Nev4XriCRm8cdfRkz7uYtbgddjcl0amOSis'
-};
-
-module.exports = credentials;
